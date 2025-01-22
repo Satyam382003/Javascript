319b2591f855b756d78b62fd67df6ddabe407e80
Satyam382003 <140716707+Satyam382003@users.noreply.github.com>
2025-01-22 12:56:19 +0000

    container config done

--- 01_Basics/App.js
+++ 01_Basics/App.js
@@ -1,1 +1,19 @@
-console.log("satyam");
+const name = "satyam"
+const surname = "pandey"
+
+console.log(`My name is ${name} and i'm here to learn something and my surname is ${surname} `);
+
+const gameName = new String('satyam-S-p')
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(5))
+// console.log(gameName.indexOf('s'))
+// console.log(gameName.lastIndexOf())
+const newString = gameName.substring(0,4)
+console.log(newString)
+const anotherString = gameName.slice(-3,2)
+console.log(anotherString)
+const newStringOne = "    satyam    ";
+console.log(newStringOne.trim())
---
+++ 01_Basics/Mapp.js
@@ -0,0 +1,34 @@
+const number = 500
+// console.log(number);
+
+// console.log(typeof 500)
+
+const balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(typeof balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2)) //How many zeroes should be there after a decimal point
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))  //en-IN means english to INR
+
+// console.log(Math);
+// console.log(Math.abs(-10))
+// console.log(Math.round(5.6)) //greater than .5
+
+// console.log(Math.ceil(5.6)) //upper value
+// console.log(Math.floor(5.6)) //Lower value
+// console.log(Math.min(3,5,2,10))
+// console.log(Math.max(5,6,10,7,19))
+
+console.log(Math.random())
+console.log(Math.random()*10 + 1)
+console.log(Math.floor(Math.random()*10) + 1)
+
+const max = 10;
+const min = 1;
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
+

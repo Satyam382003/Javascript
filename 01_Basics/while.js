04468b4e3777b6de02c7d2a455d361e3009f25c5
Satyam382003 <140716707+Satyam382003@users.noreply.github.com>
2025-01-24 19:13:40 +0000

    done

---
+++ 01_Basics/while.js
@@ -0,0 +1,22 @@
+//while loop
+
+// let i = 1;
+// while (i<=20) {
+//     console.log(`Value of index is ${i}`);
+//     i = i+2;
+// }
+
+//do-while 
+// let score = 1;
+
+// do {
+//     console.log(`score is ${score}`);
+//     score++;
+// } while (score <= 10);
+
+let arr = 1;
+
+do {
+    console.log(`score is ${arr}`);
+    arr++;
+} while (arr <= 10);
